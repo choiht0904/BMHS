7ed3c9c547d47d1fe791d435a51f0c4e7ba6ddc1
choiht0904 <choiht0904@naver.com>
2023-02-28 21:56:35 +0900

    프로젝트 생성

---
+++ businessmanager/src/test/java/com/example/businessmanager/BusinessmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.businessmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
